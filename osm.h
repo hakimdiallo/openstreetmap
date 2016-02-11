527c71602e7c9eedb44b4100c17730ddcac4b1c6
DIALLO souleymane <diallos@informatique.univ-paris-diderot.fr>
2016-02-11 16:55:30 +0100

    Déclaration des fonctions de parcours, d'ouverture, de fermeture et d'affichage des noeuds du fichier xml

--- osm.h
+++ osm.h
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+
+typedef void (*fct_parcours_t)(xmlNodePtr);//pointeur sur une fonction qui retourne un type int
 
 typedef struct tag {
 	char *key;
@@ -21,15 +25,24 @@ typedef struct node {
 	int lon;
 	int lat;
 	attributs at;
-	tag t;
+	tag t[];
 } node;
 
 typedef struct way {
 	attributs at;
 	int *refs;
-	tag *t;
+	tag t[];
 } way;
 
 typedef struct area {
 	way w;
-}
+}area;
+
+xmlDocPtr parse_file(char *name); // Parse un document xml et retourne un pointeur sur le document xml
+xmlNodePtr get_root(xmlDocPtr doc); // retourne le noeud racine s'il existe ou null sinon
+void free_file(xmlDocPtr doc); // Libere le fichier xml (Libère la mémoire)
+void parcours_prefixe(xmlNodePtr noeud, fct_parcours_t f); // Parcours en profondeur des noeuds du fichier xml
+void afficher(xmlNodePtr noeud);
+
+
+
