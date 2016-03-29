68399db3256b71d5164e17a4dff2fac284d094b0
Jules <julesziriga@gmail.com>
2016-03-29 23:14:17 +0200

    Utilisation de glib GHashtable et GSList
    my_way a une hashtable de tag et une liste de noeud. un noeud a une hashtable de tag.
    Rédéfinition des fonctions pour ajouter des éléments dans une hashtable de way, de noeuds.
    création d'une structure rélation (à completer)

--- osm_structure.c
+++ osm_structure.c
@@ -1,142 +1,70 @@
 #include "osm_structure.h"
-#include <stdlib.h>
-#include <stdio.h>
+
+
+#define SIZE_NODES 20
 
 my_way* init_my_way(){
-  my_way *way = NULL;
-  way  = malloc(sizeof(my_way));
-  way->nodes = malloc(MY_NODE_SIZE * sizeof(my_node));
-  way->tag = malloc(MY_TAG_SIZE * sizeof(my_tag));
-  way->count_nodes = 0;
-  way->count_tag = 0;
-  way->size_tag = MY_TAG_SIZE;
-  way->size_nodes = MY_NODE_SIZE;
+  my_way *way  = malloc(sizeof(my_way));
+  way->nodes = NULL;
+  way->tag = g_hash_table_new( g_str_hash, g_str_equal);
   return way;
 }
 
-void add_tag_my_way(my_way *way,my_tag tag){
-  if( (way->size_tag) <= (way->count_tag) ){
-    way->size_tag += MY_TAG_SIZE;
-    way->tag = realloc(way,way->size_tag * sizeof(my_tag));
-  }
-  way->tag[way->count_tag]=tag;
-  (way->count_tag)++;
-}
-
-void add_node_my_way(my_way *way,my_node node){
-  if( (way->size_nodes) == (way->count_nodes) ){
-    way->size_nodes += MY_NODE_SIZE;
-    way->nodes = realloc(way->nodes,way->size_nodes*sizeof(my_node));
-  }
-  way->nodes[way->count_nodes] = node;
-  (way->count_nodes)++;
-}
-
-void free_my_way(my_way *way){
-  free(way->nodes);
-  free(way->tag);
-  free(way);
-}
-
 my_node* init_my_node(){
-  my_node *node = NULL;
-  node = malloc(sizeof(my_node));
-  node->tag=malloc(MY_TAG_SIZE*sizeof(my_tag));
-  node->count_tag = 0;
-  node->size_tag = MY_TAG_SIZE;
+  my_node *node = malloc(sizeof(my_node));
+  node->tag = g_hash_table_new( g_str_hash, g_str_equal);
   return node;
 }
 
-void free_my_ways(my_way **ways, int n){
-  int i;
-  for (i=0 ; i<n; i++  ){
-    free_my_way(ways[i]);
-  }
+my_bounds* init_my_bounds(){
+  my_bounds* bound = malloc(sizeof(my_bounds));
+  return bound;
 }
 
-void add_tag_my_node(my_node *node, my_tag tag){
-  if( (node->size_tag) == (node->count_tag) ){
-    node->size_tag += MY_TAG_SIZE;
-    node->tag = realloc(node->tag,node->size_tag * sizeof(my_tag));
+void add_tag_my_way(my_way *way, my_tag *tag){
+  g_hash_table_insert(way->tag, &(tag->key), &(tag->value));
+}
+
+void add_node_my_way(my_way *way, char *idNode){
+  way->nodes = g_slist_append (g_slist_copy(way->nodes), idNode);
+}
+
+void add_tag_my_node(my_node *node, my_tag *tag){
+  g_hash_table_insert(node->tag, &(tag->key), &(tag->value));
+}
+
+void free_my_way(my_way *way){
+  g_slist_free(way->nodes);
+  g_hash_table_destroy(way->tag);
+  free(way);
+}
+
+void free_my_ways(GHashTable *ways){
+  GHashTableIter iter;
+  gpointer value;
+  g_hash_table_iter_init(&iter, ways);
+  while(g_hash_table_iter_next(&iter, NULL, &value)) {
+       free_my_way(value);
   }
-  node->tag[node->count_tag] = tag;
-  (node->count_tag)++;
+  g_hash_table_destroy(ways);
+}
+
+
+void free_my_nodes(GHashTable *nodes){
+  GHashTableIter iter;
+  gpointer value;
+  g_hash_table_iter_init(&iter, nodes);
+  while(g_hash_table_iter_next(&iter, NULL, &value)) {
+       free_my_way(value);
+  }
+  g_hash_table_destroy(nodes);
 }
 
 void free_my_node(my_node *node){
-  free(node->tag);
+  g_hash_table_destroy(node->tag);
   free(node);
 }
 
-my_hashmap_node* init_hashmap(int startsize){
-  my_hashmap_node* hm = (my_hashmap_node*)malloc(sizeof(my_hashmap_node));
-  hm->table = (hashmap_entry*)calloc(sizeof(hashmap_entry), startsize);
-  hm->size = startsize;
-  hm->count = 0;
-  return hm;
-}
-
-void add_node_hashmap(my_hashmap_node* hash, my_node *node, unsigned long id){
-  int index, i, step;
-  printf("boucle ajout\n");
-  do
-  {
-    index = id % hash->size;
-    step = (id % (hash->size-2)) + 1;
-
-    for (i = 0; i < hash->size; i++){
-      if (hash->table[index].flags & ACTIVE)
-      {
-        if (hash->table[index].id == id)
-        {
-          hash->table[index].node = node;
-          return;
-        }
-      }
-      else
-      {
-        hash->table[index].flags |= ACTIVE;
-        hash->table[index].node = node;
-        hash->table[index].id = id;
-        ++hash->count;
-        return;
-      }
-      index = (index + step) % hash->size;
-    }
-  }
-  while (1);
-}
-
-my_node* get_hashmap_node(my_hashmap_node* hash, unsigned long id)
-{
-  //Si l'hashmap contient un élément
-  if (hash->count)
-  {
-    int index, i, step;
-    index = id % hash->size;
-    step = (id % (hash->size-2)) + 1;
-
-    for (i = 0; i < hash->size; i++)
-    {
-      if (hash->table[index].id == id)
-      {
-        if (hash->table[index].flags & ACTIVE)
-          return hash->table[index].node;
-        break;
-      }
-      else
-        if (!hash->table[index].node)
-          break;
-
-      index = (index + step) % hash->size;
-    }
-  }
-
-  return 0;
-}
-
-void free_hashmap_node(my_hashmap_node* hash)
-{
-  free(hash->table);
-  free(hash);
+void free_my_bound(my_bounds *bound){
+  free(bound);
 }
--- osm_structure.h
+++ osm_structure.h
@@ -1,17 +1,23 @@
 #ifndef _OSM_STRUCTURE_H_
 	#define _OSM_STRUCTURE_H_
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <glib.h>
+
 
 #define MY_TAG_SIZE 20
 #define MY_NODE_SIZE 100
 #define ACTIVE 1
 
+
 typedef struct my_tag {
-	char key[1024];
-	char value[1024];
+	char key[200];
+	char value[200];
 } my_tag;
 
 typedef struct my_attributs {
-	int id;
+	char id[20];
 	char visible[10];
 	/*
 		char *user;
@@ -25,20 +31,21 @@ typedef struct my_node {
 	double lon;
 	double lat;
 	my_attributs at;
-	my_tag *tag;
-	int size_tag;
-	int count_tag;
+	GHashTable *tag;
 } my_node;
 
 typedef struct my_way {
-	//attributs at;
-	my_node *nodes;
-	my_tag *tag;
-	int size_tag;
-	int size_nodes;
-	int count_nodes;
-	int count_tag;
+	my_attributs at;
+	GSList *nodes;
+	GHashTable *tag;
 } my_way;
+
+typedef struct my_relation{
+	my_attributs at;
+	GSList *ways;
+	GSList *nodes;
+	GSList *relation;
+}my_relation;
 
 typedef struct my_area {
 	my_way w;
@@ -51,55 +58,36 @@ typedef struct my_bounds{
 	double minlon;
 }my_bounds;
 
-// l'entree de l'hashmap
-typedef struct hashmap_entry{
-	unsigned long id;
-	my_node *node;
-	int flags;
-} hashmap_entry;
-
-
-typedef struct my_hashmap_node{
-	hashmap_entry *table;
-	int size, count;
-} my_hashmap_node;
-
 //Innitialise la structure my_way
 my_way* init_my_way();
-
-//ajoute un tag dans un way
-void add_tag_my_way(my_way *way,my_tag tag);
-
-//ajoute un noeud dans un way
-void add_node_my_way(my_way *way,my_node node);
-
-//détruit la structure my_way
-void free_my_way(my_way *way);
 
 //Initialise un noeud
 my_node* init_my_node();
 
+//Initialise un bound
+my_bounds* init_my_bounds();
+
+//ajoute un tag dans un way
+void add_tag_my_way(my_way *way,my_tag *tag);
+
+//ajoute un noeud dans un way
+void add_node_my_way(my_way *way, char *idNode);
+
+//ajoute un tag dans un node
+void add_tag_my_node(my_node *node, my_tag *tag);
+
+//détruit la structure my_way
+void free_my_way(my_way *way);
+
+//Libère un tableau de ways
+void free_my_ways(GHashTable *ways);
+
 //Libère la structure my_node
 void free_my_node(my_node *node);
 
-//ajoute un tag dans un node
-void add_tag_my_node(my_node *node, my_tag tag);
+//Libère l'hashmap de noeuds
+void free_my_nodes(GHashTable *nodes);
 
-//Libère un tableau de ways
-void free_my_ways(my_way **ways, int nombre_de_way);
-
-
-// initialise un hashmap avec une taille donnée
-my_hashmap_node* init_hashmap(int startsize);
-
-//Ajoute un noeud dans l'hashmap
-void add_node_hashmap (my_hashmap_node* hash, my_node *node, unsigned long id);
-
-//Recupère le my_node qui a l'id entrée en paramètre
-my_node* get_hashmap_node(my_hashmap_node* hash, unsigned long id);
-
-//Libère la structure hashmap
-void free_hashmap_node(my_hashmap_node* hash);
-
-
+//Libère la structure my_bounds
+void free_my_bound(my_bounds *bound);
 #endif
